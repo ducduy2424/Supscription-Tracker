8d5341a5ad4e817cf8b2bdaa842dbc25a61a4d4c
ducduy2424 <duynvdd2424@gmail.com>
2025-08-11 02:47:52 +0700

    Setup enviroment variables, and update .gitignore

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from 'express'
+import { PORT } from './config/.env.js';
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Welcome to Supcription Tracker API');
 })
 
-app.listen(3000, () => {
-    console.log('Supcrition Tracker API is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Supcrition Tracker API is running on http://localhost:${PORT}`);
 })
 
 export default app;
